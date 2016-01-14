02303f50d1fdfaf09184ff3fd416b463e867ea98
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-01-14 20:42:18 +0100

    widełki cenowe dla kosztów jednostkowych

--- graMain.cpp
+++ graMain.cpp
@@ -4,14 +4,34 @@ using namespace std;
 
 int main()
 {
-	cout<<"podaj ilość gotówki"<<endl;
 	double kasa;
-	double koszt_jednostkowy=9.83;
-	cin>>kasa;
-	cout<<"cena: 22"<<endl;
-	cout<<"jakość: 55"<<endl;
-	double wolumen=kasa/koszt_jednostkowy;
-	cout<<"wolumen: "<<wolumen<<endl;
+	int i=0;
+	double koszt_jednostkowy[]={9.83,//kasa ~ 300000
+								9.49,//kasa ~ 510000
+								9.44,//900000
+								9.85,//1700000
+								10.95//3270000
+								};
+	double wolumen;
+	while(true)
+	{
+		cout<<"podaj ilość gotówki"<<endl;
+		cin>>kasa;
+		if(kasa<400000)
+			i=0;
+		else if(kasa<700000)
+			i=1;
+		else if(kasa<1300000)
+			i=2;
+		else if(kasa<2485000)
+			i=3;
+		else
+			i=4;
+		cout<<"cena: 22"<<endl;
+		cout<<"jakość: 55"<<endl;
+		wolumen=kasa/koszt_jednostkowy[i];
+		cout<<"wolumen: "<<wolumen<<endl;
+	}
 
 	return 0;
 }
