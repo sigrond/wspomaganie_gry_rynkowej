bc9104debd7d387096df4cbb87ab77337b95928f
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-01-14 20:07:06 +0100

    proste podpowiedzi

--- graMain.cpp
+++ graMain.cpp
@@ -6,7 +6,12 @@ int main()
 {
 	cout<<"podaj ilość gotówki"<<endl;
 	double kasa;
+	double koszt_jednostkowy=9.83;
 	cin>>kasa;
+	cout<<"cena: 22"<<endl;
+	cout<<"jakość: 55"<<endl;
+	double wolumen=kasa/koszt_jednostkowy;
+	cout<<"wolumen: "<<wolumen<<endl;
 
 	return 0;
 }
